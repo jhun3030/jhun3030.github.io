b0961cbfde01fe80325d0abbf82193214926d5b2
Jaeheon Kim <jhun3030@gmail.com>
2022-03-03 01:29:53 +0900

    JS : navbar menu click

--- main.js
+++ main.js
@@ -5,11 +5,22 @@ const navBar = document.querySelector(`#navbar`);
 const navBarHeight = navbar.getBoundingClientRect().height;
 
 document.addEventListener(`scroll`, function () {
-  console.log(window.scrollY);
-  console.log(`navbar height ${navBarHeight}`);
   if (window.scrollY > navBarHeight) {
     navBar.classList.add(`navbar--dark`);
   } else {
     navBar.classList.remove(`navbar--dark`);
   }
+});
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector(`.navbar__menu`);
+navbarMenu.addEventListener(`click`, function (event) {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link === undefined) {
+    return;
+  }
+  console.log(link);
+  const scrollTo = document.querySelector(link);
+  scrollTo.scrollIntoView({ behavior: `smooth` });
 });
